f315ad3263cf0567e1e93c421ad53d2016f501c8
lelegoyeng <lelegoyengs@gmail.com>
2023-10-27 16:12:15 +0700

    Update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.png';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { totalStorage } = props
     return (
         <div className="Navbar">
             <div className="bg-black text-white p-4 flex flex-col md:flex-row items-center justify-between">
@@ -15,18 +16,15 @@ const Navbar = () => {
                         }}
                     >
                         <img src={logo} alt="Logo" className="h-8 w-8" />
-                        <span className="ml-2 text-lg font-bold">Grive Management</span>
+                        <span className="ml-2 text-lg font-bold">Grive Management Storage</span>
                     </Link>
 
                 </div>
 
                 {/* Centered Search Input */}
                 <div className="flex items-center w-full md:w-auto mb-4 md:mb-0">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className="w-full md:w-auto bg-gray-800 text-white p-2 rounded focus:outline-none"
-                    />
+                    <span className="ml-2 text-lg font-bold">Total Storage: {totalStorage} GB</span>
+
                 </div>
 
                 {/* Options in the Top Right Corner */}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 const Home = () => {
     const [AccountInformation, setAccountInformation] = useState([]);
@@ -30,21 +28,46 @@ const Home = () => {
         },
     ]
 
-    console.log(AccountInformation)
+    const totalStorage = AccountInformation?.reduce((a, b) => a + b.storageQuota?.totalStorage, 0);
+    const bytes = totalStorage;
+    const gigabytes = (bytes / 1073741824).toFixed(2);
     return (
         <div>
-            <Navbar />
+            <Navbar totalStorage={gigabytes} />
             <div className='justify-center px-10 py-10 h-full w-full min-h-screen min-w-screen bg-gray-800 overflow-y-auto'>
-                <div className="grid grid-cols-1 md:grid-cols-12 lg:grid-cols-6 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-12 lg:grid-cols-3 gap-4">
                     {
-                        AccountInformation?.AccountConnected?.map((value, index) => {
-                            return <div key={index} className="bg-black text-white font-bold p-4 rounded-md shadow-md">
-                                Card {value}
+                        AccountInformation?.map((value, index) => {
+                            const Usage = (value?.storageQuota?.usageStorage / 1073741824).toFixed(2);
+                            const Storage = (value?.storageQuota?.totalStorage / 1073741824).toFixed(2);
+
+                            return <div key={index} className="bg-black text-white font-bold p-4 rounded-md shadow-md break-all">
+                                <div className='flex justify-between'>
+                                    <div><img className='rounded' src={value?.checkConnection?.photoLink}></img></div>
+                                    <div className='mr-1 ml-1'>Google Account</div>
+                                    <div> </div>
+                                </div>
+                                <div className='flex justify-between mt-2'>
+                                    <div className='w-1/4'>Email</div>
+                                    <div className='mr-2 ml-2'>:</div>
+                                    <div className='w-3/4'> {value?.checkConnection?.emailAddress}</div>
+                                </div>
+                                <div className='flex justify-between mt-2'>
+                                    <div className='w-1/4'>Usage</div>
+                                    <div className='mr-2 ml-2'>:</div>
+                                    <div className='w-3/4'> {Usage} GB </div>
+                                </div>
+                                <div className='flex justify-between mt-2'>
+                                    <div className='w-1/4'>Storage</div>
+                                    <div className='mr-2 ml-2'>:</div>
+                                    <div className='w-3/4'> {Storage} GB </div>
+                                </div>
                             </div>
                         })
                     }
                 </div>
             </div>
+
         </div>
     );
 }
