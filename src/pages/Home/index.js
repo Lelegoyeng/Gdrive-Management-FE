4e76fa283fc04ae303be42d70cc988320dd91ca8
lelegoyeng <lelegoyengs@gmail.com>
2023-10-27 15:26:16 +0700

    Fetch fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-
+import axios from 'axios';
 const Home = () => {
+    const [AccountInformation, setAccountInformation] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/accounts');
+                setAccountInformation(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [])
+
     const ArrayCards = [
         {
             Account: 'Bot-1',
@@ -16,32 +30,17 @@ const Home = () => {
         },
     ]
 
-    ArrayCards.push({
-        Account: 'newAccount',
-        Storage: '2GB'
-    });
-
+    console.log(AccountInformation)
     return (
         <div>
             <Navbar />
             <div className='justify-center px-10 py-10 h-full w-full min-h-screen min-w-screen bg-gray-800 overflow-y-auto'>
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-12 lg:grid-cols-6 gap-4">
                     {
-                        ArrayCards.map((value, index) => {
-                            if (value.Account !== 'newAccount') {
-                                return <div key={index} className="bg-black text-white font-bold p-4 rounded-md shadow-md">
-                                    Card {value.Account}
-                                </div>
-                            }
-                            if (value.Account === 'newAccount') {
-                                return <div
-                                    key={index}
-                                    className="bg-black text-white font-bold p-4 rounded-md shadow-md cursor-pointer hover:bg-blue-500">
-                                    <FontAwesomeIcon icon={faPlus} className="mr-2" />
-                                    New Account
-                                </div>
-                            }
-
+                        AccountInformation?.AccountConnected?.map((value, index) => {
+                            return <div key={index} className="bg-black text-white font-bold p-4 rounded-md shadow-md">
+                                Card {value}
+                            </div>
                         })
                     }
                 </div>
